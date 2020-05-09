5f15a120e45e80667d947fb716aaece2d9822e17
Kawalya Andrew <andrew58934@gmail.com>
2020-05-09 08:58:29 +0300

    #123-fixed cart total

--- lib/pages/shopping_cart_screen.dart
+++ lib/pages/shopping_cart_screen.dart
@@ -1,8 +1,13 @@
+import 'package:ecommerce_app/model/cart_model.dart';
+import 'package:ecommerce_app/provider/product_provider2.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:ecommerce_app/componants/cart_products.dart';
+import 'package:ecommerce_app/componants/cart-product-detail.dart';
+import 'package:provider/provider.dart';
 
 class ShoppingCart extends StatefulWidget {
   static const String id = 'shoppingCart';
+
   @override
   _ShoppingCartState createState() => _ShoppingCartState();
 }
@@ -10,70 +15,144 @@ class ShoppingCart extends StatefulWidget {
 class _ShoppingCartState extends State<ShoppingCart> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'My Cart',
-          style: TextStyle(
-            color: Colors.black54,
-            fontWeight: FontWeight.w900,
-            fontFamily: 'Poppins',
-            fontSize: 24.0,
-          ),
-        ),
-        backgroundColor: Colors.white,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              // appProvider.myFeatureProduct();
-            },
-          )
-        ],
-        iconTheme: IconThemeData(color: Colors.black54),
-        elevation: 0.0,
-      ),
-      bottomNavigationBar: Container(
-        color: Colors.white,
-        padding: EdgeInsets.all(5.0),
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: ListTile(
-                title: Text(
-                  'Total',
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+    //var data = Provider.of<ProductProvider2>(context);
+    //double totalPrice = data.getTotalPrice();
+
+    //List<CartModel> cartList = data.cartProductList;
+
+    var getValue = Provider.of<ProductProvider2>(context);
+
+    List<CartModel> cartList = getValue.cartProductList;
+    double totalPrice = getValue.getTotalPrice();
+
+    return cartList.length == 0
+        ? Scaffold(
+            appBar: AppBar(
+              title: Text(
+                'My Cart',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'Poppins',
+                  fontSize: 24.0,
                 ),
-                subtitle: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Text(
-                    '\$185',
-                    style:
-                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              backgroundColor: Colors.white,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    // appProvider.myFeatureProduct();
+                  },
+                )
+              ],
+              iconTheme: IconThemeData(color: Colors.black54),
+              elevation: 0.0,
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 300,
+                  width: 300,
+                  child: Image.asset(
+                    'images/cart/empty_cart.jpeg',
+                    fit: BoxFit.contain,
                   ),
                 ),
+                Text(
+                  'Your Cart Is Empty\n Add Items',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ],
+            ))
+        : Scaffold(
+            appBar: AppBar(
+              title: Text(
+                'My Cart',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'Poppins',
+                  fontSize: 24.0,
+                ),
+              ),
+              backgroundColor: Colors.white,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    // appProvider.myFeatureProduct();
+                  },
+                )
+              ],
+              iconTheme: IconThemeData(color: Colors.black54),
+              elevation: 0.0,
+            ),
+            bottomNavigationBar: Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(5.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ListTile(
+                      title: Text(
+                        'Total',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          '\$${totalPrice.roundToDouble()}',
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Material(
+                      elevation: 2.0,
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(30.0),
+                      child: MaterialButton(
+                        onPressed: () {},
+                        minWidth: 200.0,
+                        height: 35.0,
+                        child: Text(
+                          'Checkout',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
-            Expanded(
-              child: Material(
-                elevation: 2.0,
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {},
-                  minWidth: 200.0,
-                  height: 35.0,
-                  child: Text(
-                    'Checkout',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
+            body: ListView.builder(
+              itemCount: cartList.length,
+              itemBuilder: (context, index) => CartItems(
+                name: cartList[index].name,
+                picture: cartList[index].images[0],
+                price: cartList[index].price,
+                selectedSize: cartList[index].selectedSize,
+                selectedColor: cartList[index].selectedColor,
+                quantity: cartList[index].qty,
+                buttonUp: () {
+                  getValue.increaseQty(index);
+                  //cartList[index].qty++;
+                  //assert(cartList[index].qty >= 0);
+                  //getValue.getTotalPrice();
+                },
+                buttonDown: () async {
+                  await getValue.decreaseQty(index);
+                  //assert(cartList[index].qty >= 0);
+                  //cartList[index].qty--;
+                  //getValue.getTotalPrice();
+                },
               ),
-            )
-          ],
-        ),
-      ),
-      body: ShoppingCartProducts(),
-    );
+            ),
+          );
   }
 }
--- lib/provider/product_provider2.dart
+++ lib/provider/product_provider2.dart
@@ -6,16 +6,59 @@ import 'package:flutter/material.dart';
 class ProductProvider2 with ChangeNotifier {
   List<CartModel> _cartProductList = [];
 
+  double _quantity = 1;
+  double get quantity => _quantity;
+
   List get cartProductList => _cartProductList;
 
   void addProducts(CartModel cartItem) {
-    _cartProductList.add(cartItem);
+    bool isPresent = false;
 
+    if (_cartProductList.length > 0) {
+      for (int i = 0; i > _cartProductList.length; i++) {
+        if (_cartProductList[i].id == cartItem.id) {
+          increaseItemQuantity(cartItem);
+          isPresent = true;
+          break;
+        } else {
+          isPresent = false;
+        }
+      }
+      if (!isPresent) {
+        _cartProductList.add(cartItem);
+      }
+    } else {
+      _cartProductList.add(cartItem);
+    }
+
+    notifyListeners();
+  }
+
+  double getTotalPrice() {
+    double totalPrice = 0;
+    _cartProductList.forEach((item) {
+      totalPrice += item.price * item.qty;
+    });
+
+    return totalPrice;
+  }
+
+  void increaseQty(int index) {
+    _cartProductList[index].qty++;
+    notifyListeners();
+  }
+
+  void decreaseQty(int index) {
+    _cartProductList[index].qty--;
     notifyListeners();
   }
 
   removeProduct(CartModel cartItem) {
     _cartProductList.remove(cartItem);
     notifyListeners();
+  }
+
+  void increaseItemQuantity(CartModel cartItem) {
+    cartItem.increaseQty();
   }
 }
